478bd3f34efd8112095f3ace1b61ca42a1d8362d
Hampton Rogers <rogershampton22@gmail.com>
2020-11-11 14:57:53 -0600

    Completed. Refactor if desired

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class Car : IVehicle, ICompany
+    {
+        public Car()
+        {
+        }
+
+        public double EngineSize { get; set; } = 2.5;
+        public string Make { get; set; } = "Suburu";
+        public string Model { get; set; } = "Legacy";
+        public int SeatCount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Suburu Corporation";
+        public string Motto { get; set; } = "Love. It's What Makes a Subaru, a Subaru";
+        public bool HasChangedGears { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward...");
+        }     
+        public void Reverse()
+        {
+            if(HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing...");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse untill we change gears");
+            }
+        }
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now in park...");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse untill we change gears");
+            }
+        }
+
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+    }
+}
---
+++ InterfaceExercise/SUV.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class SUV : IVehicle, ICompany
+    {
+        public SUV()
+        {
+        }
+        public double EngineSize { get; set; } = 3.6;
+        public string Make { get; set; } = "Acura";
+        public string Model { get; set; } = "RDX";
+        public int SeatCount { get; set; } = 5;
+        public string CompanyName { get; set; } = "Honda";
+        public string Motto { get; set; } = "The Power of Dreams";
+        public bool HasChangedGears { get; set; }
+
+        public bool HasFourWheelDrive { get; set; } = false;
+
+        public void Drive()
+        {
+            if (HasFourWheelDrive == true)
+            {
+                Console.WriteLine($"4 wheel drive {GetType().Name} now driving forward...");
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} now driving forward...");
+            }
+        }
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing...");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse untill we change gears");
+            }
+        }
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now in park...");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse untill we change gears");
+            }
+        }
+
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+    }
+}
